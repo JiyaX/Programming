b68e7788565b3e144968209c291301e8ed4413d7
JiyaX <39413441+JiyaX@users.noreply.github.com>
2019-12-23 20:06:56 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin<<n;
+    for(int i=2;i<=n;i++)56
+    {
+        for(int j=2;j<=i;j++)
+        {
+            if(i%j==0)
+        }
+    }
